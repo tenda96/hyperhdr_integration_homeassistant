5bacb740cf5a6477428ea527f1e9f69ed4f13a44
tenda96 <luca96.tenderini@gmail.com>
2026-05-25 00:55:16 +0200

    Add files via upload
    hacs fix

---
+++ custom_components/hyperhdr_integration/const.py
@@ -0,0 +1,13 @@
+"""Constants for the HyperHDR Home Assistant integration."""
+
+DOMAIN = "hyperhdr_integration"
+DEFAULT_PORT = 8090
+DEFAULT_NAME = "HyperHDR"
+DEFAULT_PRIORITY = 50
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_TOKEN = "token"
+CONF_NAME = "name"
+
+UPDATE_INTERVAL = 5
